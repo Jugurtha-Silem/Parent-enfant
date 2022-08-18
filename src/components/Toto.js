23f5f677d50f90f8623485274583736d2038c4c3
Jugurtha.Silem <jugurtha@Mac-mini-de-Jugurtha.local>
2022-08-18 15:27:10 +0200

    parentenfantV1

---
+++ src/components/Toto.js
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+    
+
+const Toto = (props) => {
+
+
+    const btnReponseToto = props.leState.messageMaman !== null ? (<button onClick={props.reponseToto}>Réponse</button>) : (<button disabled>Réponse</button>) 
+
+
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            {btnReponseToto}
+
+            <p>{props.leState.messageToto}</p>
+        </div>
+    )
+
+} 
+
+export default Toto;
